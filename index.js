e2512003d72ac9e5405c6f52c214db38ecd0b4ee
Gerardo <gallegosg13@gmail.com>
2024-03-24 10:27:00 -0700

    add some stuff

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 import axios from "axios";
-
+import insulter from "insult";
 // const authenticate = require("@xboxreplay/xboxlive-auth");
 
 client.on("ready", () => {
@@ -18,7 +18,7 @@ client.on("ready", () => {
 
 async function getMeme() {
   try {
-    const res = await axios.get("https://meme-api.herokuapp.com/gimme");
+    const res = await axios.get("https://meme-api.com/gimme");
     return res.data.url;
   } catch (e) {
     return "Oops idk what happened";
@@ -42,45 +42,94 @@ async function getSubredditImage(sub = "wrx") {
   }
 }
 
+const getJoke = async () => {
+  try {
+    const res = await axios.get("https://v2.jokeapi.dev/joke/Dark");
+    const { data } = res;
+    const output = { joke: "", punchline: "" };
+
+    if (data.joke) {
+      output.joke = data.joke;
+    } else if (data.setup && data.delivery) {
+      output.joke = data.setup;
+      output.delivery = data.delivery;
+    }
+    return output;
+  } catch (_) {
+    return "Uh oh, nothing happened";
+  }
+};
+
 client.on("messageCreate", async (msg) => {
-  switch (msg.content) {
-    case "ping":
-      msg.reply("Pong!");
-      break;
-    //our meme command below
-    case "!meme":
-      msg.channel.send("Here's your meme!"); //Replies to user command
-      const img = await getMeme();
-      msg.channel.send(img); //send the image URL
-      break;
-    case "!on":
-      const message =
-        "@everyone https://tenor.com/view/the-goon-cod-warzone-call-of-duty-hurry-up-jump-on-gif-17162875";
-      msg.channel.send(message).then((sentEmbed) => {
-        sentEmbed.react("🙌");
-      });
-      break;
-    case "!wt":
-      msg.channel.send("@everyone What time?").then((sentEmbed) => {
-        sentEmbed.react("🕔");
-        sentEmbed.react("🕠");
-        sentEmbed.react("🕕");
-        sentEmbed.react("🕡");
-        sentEmbed.react("🕖");
-      });
-      break;
-    case "!rnd":
-      msg.reply("Type a subreddit to get a random post from it ex: `!rnd wrx`"); //Replies to user command
-      break;
-    default:
-      if (msg.content.startsWith("!rnd ")) {
-        const sub = msg.content.split("!rnd ")[1];
-        if (sub) {
-          msg.channel.send("Here's a random pic from r/" + sub); //Replies to user command
-          const redditImage = await getSubredditImage(sub);
-          msg.channel.send(redditImage); //send the image URL
+  try {
+    if (msg.mentions.has(client.user)) {
+      msg.reply(insulter.Insult());
+      return;
+    }
+    switch (msg.content) {
+      case "ping":
+        msg.reply("Pong!");
+        break;
+      //our meme command below
+      case "!meme":
+        msg.channel.send("Here's your meme!"); //Replies to user command
+        const img = await getMeme();
+        msg.channel.send(img); //send the image URL
+        break;
+      case "!on":
+        const message =
+          "@everyone https://tenor.com/view/the-goon-cod-warzone-call-of-duty-hurry-up-jump-on-gif-17162875";
+        msg.channel.send(message).then((sentEmbed) => {
+          sentEmbed.react("🙌");
+        });
+        break;
+      case "!wt":
+        msg.channel.send("@everyone What time?").then((sentEmbed) => {
+          sentEmbed.react("🕔");
+          sentEmbed.react("🕠");
+          sentEmbed.react("🕕");
+          sentEmbed.react("🕡");
+          sentEmbed.react("🕖");
+        });
+        break;
+      case "!rnd":
+        msg.reply(
+          "Type a subreddit to get a random post from it ex: `!rnd wrx`"
+        ); //Replies to user command
+        break;
+      case "!joke":
+        const { joke, delivery } = await getJoke();
+        msg.reply(joke).then((sent) => {
+          if (delivery) {
+            setTimeout(() => {
+              sent.reply(delivery);
+            }, 3000);
+          }
+        });
+        break;
+      case "!help":
+        msg.channel.send(`ping - check if the server is up. will return pong
+      !meme - generates a random meme
+      !on - let everyone know you are onine and ready to play
+      !wt - ask the group what time they are going to get on to play
+      !rnd {subreddit} - returns a random post from the subreddit specified
+      !help - returns this list of commands supported
+      !joke - tells a joke
+      @Botty - chat with botty
+      `);
+        break;
+      default:
+        if (msg.content.startsWith("!rnd ")) {
+          const sub = msg.content.split("!rnd ")[1];
+          if (sub) {
+            msg.channel.send("Here's a random pic from r/" + sub); //Replies to user command
+            const redditImage = await getSubredditImage(sub);
+            msg.channel.send(redditImage); //send the image URL
+          }
         }
-      }
+    }
+  } catch {
+    msg.channel.send("uh");
   }
 });
 
